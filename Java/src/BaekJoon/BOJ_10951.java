a749b70ef9e6240820f0188526fa7109ec87d9f9
김정윤 <kimjy199@naver.com>
2021-12-15 02:42:39 +0900

    add BaekJoon A+B - 4

---
+++ Java/src/BaekJoon/BOJ_10951.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BOJ_10951 {
+
+	/**
+	 * 문제
+	 *  A+B 출력하기
+	 *   - 테스트케이스 개수가 주어지지 않는다.
+	 *  => 더 이상 입력 데이터가 없으면 종료
+	 *   
+	 * 시간 : 136 ms
+	 * 메모리 : 13092 kb
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+		}
+
+	}
+
+}
