5fc956f6b295dc0e936e20ec355badea5c061b4d
김정윤 <kimjy199@naver.com>
2022-01-19 03:25:05 +0900

    add SWEA 대각선 출력하기

---
+++ Java/src/SWEA/SWEA_2027.java
@@ -0,0 +1,20 @@
+package SWEA;
+
+public class SWEA_2027 {
+
+	/**
+	 * 문제
+	 *  주어진 텍스트를 그대로 출력하기
+	 * 
+	 * 시간: 99 ms
+	 * 메모리: 18,740 kb
+	 */
+	public static void main(String[] args) {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+	}
+
+}
