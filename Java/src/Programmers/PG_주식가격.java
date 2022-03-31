2508f166b6bf0ed9145b44b6921d2f5a13135f5f
김정윤 <kimjy199@naver.com>
2022-03-31 23:54:16 +0900

    add Programmers 주식가격

---
+++ Java/src/Programmers/PG_주식가격.java
@@ -0,0 +1,36 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class PG_주식가격 {
+
+	/**
+	 * 문제
+	 *  초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 각 초의 주가를 기준으로 가격이 떨어지지 않은 기간은 몇 초인지를 구하라.
+	 *   - prices의 각 가격은 1 이상 10,000 이하인 자연수
+	 *   - prices의 길이는 2 이상 100,000 이하
+	 *   
+	 *  => 연속적으로 가격이 유지된 기간. 즉, 주가가 바로 떨어진 시점의 시간을 구한다.
+	 *  => 떨어진 이후는 고려하지 않는다.
+	 */
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(new int[] {1, 2, 3, 2, 3}))); // 이중for문
+	}
+	
+	// 이중 for문을 활용한 방법. 시간 복잡도는 N²
+	// 효율성 테스트 1 〉	통과 (19.31ms, 85.2MB)
+	public static int[] solution(int[] prices) {
+		int size = prices.length;
+        int[] answer = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+			for (int j = i+1; j < size; j++) {
+				// 떨어지기 전까지 1초동안은 prices[i] 값을 유지하고 있다.
+				answer[i]++;
+				// 떨어진 이후는 고려하지 않음
+				if(prices[i] > prices[j]) break;
+			}
+		}
+        return answer;
+    }
+}
