edf1e257bee62beb93c119187110fc347525af3c
김정윤 <kimjy199@naver.com>
2022-04-11 23:59:50 +0900

    update Programmers 주식가격-2번째 Solution

--- Java/src/Programmers/PG_주식가격.java
+++ Java/src/Programmers/PG_주식가격.java
@@ -1,6 +1,8 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class PG_주식가격 {
 
@@ -12,14 +14,16 @@ public class PG_주식가격 {
 	 *   
 	 *  => 연속적으로 가격이 유지된 기간. 즉, 주가가 바로 떨어진 시점의 시간을 구한다.
 	 *  => 떨어진 이후는 고려하지 않는다.
+	 *  => 해결 방법은 2가지(이중for문, stack)
 	 */
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(solution(new int[] {1, 2, 3, 2, 3}))); // 이중for문
+		System.out.println(Arrays.toString(solution1(new int[] {1, 2, 3, 2, 3}))); // 이중for문
+		System.out.println(Arrays.toString(solution2(new int[] {1, 2, 3, 2, 3}))); // stack
 	}
 	
 	// 이중 for문을 활용한 방법. 시간 복잡도는 N²
 	// 효율성 테스트 1 〉	통과 (19.31ms, 85.2MB)
-	public static int[] solution(int[] prices) {
+	public static int[] solution1(int[] prices) {
 		int size = prices.length;
         int[] answer = new int[size];
         
@@ -33,4 +37,34 @@ public class PG_주식가격 {
 		}
         return answer;
     }
+	
+	// stack을 활용한 방법
+	public static int[] solution2(int[] prices) {
+		int size = prices.length;
+        int[] answer = new int[size];
+        
+		// stack에 시간 저장하기
+		Stack<Integer> stack = new Stack<>();
+		
+        int i = 0;
+        stack.push(i);
+        
+		for (i = 0; i < size; i++) {
+			// 스택이 비지 않고, 주가가 떨어진다면
+			while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+				int top = stack.pop();
+				answer[top] = i - top;
+			}
+			// 현재 시간 저장. idx값으로 저장한다.
+			stack.add(i);
+		}
+		
+		// 스택이 남아있다면..
+		while(!stack.isEmpty()) {
+			int top = stack.pop();
+			// 끝에 도달한 시간(size-1)에서 top만큼 빼기
+			answer[top] = size - 1 - top;
+		}
+		return answer;
+	}
 }
