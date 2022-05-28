0c1291ece2c3aacedd2858e0db1a4c6ec48d8890
김정윤 <kimjy199@naver.com>
2022-05-28 23:23:11 +0900

    update BaekJoon IOIOI **

--- Java/src/BaekJoon/BOJ_5525.java
+++ Java/src/BaekJoon/BOJ_5525.java
@@ -3,7 +3,6 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ_5525 {
 
@@ -21,28 +20,85 @@ public class BOJ_5525 {
 	 *   - 2N+1 ≤ M ≤ 1,000,000
 	 *   - S는 I와 O로만 이루어져 있다.
 	 *   
-	 * 시간: 96 ms
-	 * 메모리: 14048 kb
+	 * => 브루트포스 알고리즘으로 하면 시간 초과 발생해서 50점
+	 *   => 문자열 길이와 패턴의 길이가 길수록 시간 복잡도가 증가하게 됨
+	 * => KMP알고리즘을 사용하여 시간 줄이기
+	 *   
+	 * 시간: 232 ms
+	 * 메모리: 25320 kb
 	 */
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine()); // 1 ≤ N ≤ 1,000,000
-		int M = Integer.parseInt(br.readLine()); // 문자열 길이. 2N+1 ≤ M ≤ 1,000,000
-		String str = br.readLine();
+		int N = Integer.parseInt(br.readLine()); 
+		int M = Integer.parseInt(br.readLine()); // S의 길이
+		char[] S = br.readLine().toCharArray(); // 패턴
 		
-		String Pn = "";
-		for (int i = 0; i < 2*N+1; i++) {
-			if(i % 2 == 0) Pn += "I";
-			else Pn += "O";
+		// 찾을 패턴 구하기
+		char[] P = new char[2*N+1];
+		P[0] = 'I';
+		
+		for (int i = 1; i < 2*N+1; i+=2) {
+			P[i] = 'O';
+			P[i+1] = 'I';
 		}
 		
-		int cnt = 0;
-		for (int i = 0; i < M-(2*N+1); i++) {
-			if(str.charAt(i) == 'O') continue;
-			else if(str.substring(i, i+(2*N+1)).equals(Pn)) cnt++;
+		// KMP 알고리즘 활용하여 두 문자열(S와 P) 비교
+		KMP(S, P, M, 2*N+1);
+	}
+	
+	/**
+	 * 접두사와 접미사가 일치하는 최대길이를 구한 부분 일치 테이블 생성
+	 * @param pattern : 찾는 패턴
+	 * @param size : pattern의 길이
+	 */
+	private static int[] makeTable(char[] pattern, int size) {
+		int[] table = new int[size];
+		
+		int idx = 0;
+		for (int i = 1; i < size; i++) {
+			// 일치하는 문자가 발생했을 대(idx > 0), 연속적으로 더 일치하지 않으면 idx = table[idx-1]로 돌려준다
+			while (idx > 0 && pattern[i] != pattern[idx]) {
+				idx = table[idx-1];
+			}
+			
+			// 일치하는 경우
+			if (pattern[i] == pattern[idx]) {
+				// i길이 문자열의 공통 길이는 j의 위치 + 1 
+				idx += 1;
+				table[i] = idx;
+			}
 		}
-		System.out.println(cnt);
+
+		return table;
+	}
+	
+	private static void KMP(char[] search, char[] pattern, int s_size, int p_size) {
+		int[] table = makeTable(pattern, p_size);
+		
+		int idx = 0; // 현재 대응되는 글자 수
+		int result = 0; // search 안에 있는 pattern의 개수
+		
+		for (int i = 0; i < s_size; i++) {
+			// idx번 글자와 짚더미의 해당 글자가 불일치할 경우
+			// 현재 대응된 글자의 수를 table[idx-1]번으로 줄인다.
+			while(idx > 0 && search[i] != pattern[idx]) {
+				idx = table[idx-1];
+			}
+			
+			// 글자가 대응할 경우
+			if (search[i] == pattern[idx]) {
+				if (idx == p_size-1) {
+					idx = table[idx];
+					result++;
+				}
+				// 맞았지만 pattern이 끝나지 않았다면
+				else idx += 1;
+			}
+		}
+		
+		// 결과 출력
+		System.out.println(result);
 	}
 
 }
